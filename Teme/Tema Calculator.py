430abd1edc82b7361d9f6d68b4900fd2f35d75e7
mihai <mihai.ilie@nxdata.ro>
2023-01-03 23:12:15 +0200

    Calculator

--- Teme/Tema Calculator.py
+++ Teme/Tema Calculator.py
@@ -34,3 +34,16 @@ while True:
         print("Nu a-ti introdus semnul de operare corect")
 
 print()
+
+
+""" Daca nu introduc semnul de operare corect, programul ma lasa sa introduc mai departe primul numar si al doilea. 
+Corect ar fi sa ma atentioneze de la inceput ca nu am introdus corect, nu la sfarsit. 
+
+if operation_sign not in ['+', '-', '/', '*']:
+    print("Nu a-ti introdus semnul de operare corect")
+    continue
+    
+Daca nu introduc numar (introduc litere), programul crapa --> trebuie sa validam si sa atentionam utilizatorul ca nu a introdus
+ce trebuie si deci trebuie sa repete instructiunea.
+ 
+Daca incerc sa impart la 0, programul crapa (ar fi trebuit sa am mesaj ca nu se poate efectua impartirea la 0)"""
