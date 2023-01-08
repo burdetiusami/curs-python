d913305aef478b0fa97821305bf91319c412b583
mihai <mihai.ilie@nxdata.ro>
2023-01-08 17:23:37 +0200

    calc fctii

--- Teme/Tema Calculator Functii.py
+++ Teme/Tema Calculator Functii.py
@@ -48,3 +48,18 @@ while True:
         print("Nu a-ti introdus semnul de operare corect")
 
 print()
+
+"""Mult mai bine decat prima varianta de calculator. Bravo!
+
+ce am mai observat:
+- daca introduc o litera atunci cand trebuie sa inserez numar programul crapa. Ar trebui validat si asta:
+
+Introduceti semnul de operare: -
+Introduceti primul numar: 12
+Introduceti al doilea numar: a
+Traceback (most recent call last):
+  File "/home/mihai/Mihai/records sciit/cursuri/Repo grupa 14/Repo Studenti Grupa 14/Burdetiu Samuel/Teme/Tema Calculator Functii.py", line 34, in <module>
+    second_number = float(input("Introduceti al doilea numar: "))
+ValueError: could not convert string to float: 'a'
+
+"""
