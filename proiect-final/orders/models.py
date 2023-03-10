792fa1cd0336be2a14e38ed2ce93707f983ca982
Burdetiu <gabyburdetiu@gmail.com>
2023-03-10 13:41:28 +0100

    Proiect-final

--- proiect-final/orders/models.py
+++ proiect-final/orders/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Orders(models.Model):
+    company = models.CharField(max_length=80)
+    address = models.CharField(max_length=80)
+    cargo_info = models.CharField(max_length=100)
+    eta = models.DateField()
+    status = models.BooleanField(default=True)
--- proiect-final/orders/urls.py
+++ proiect-final/orders/urls.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from users import views
+from orders import views
 
 app_name = 'orders'
 
 urlpatterns = [
-    path('', views.UsersView.as_view(), name='orders_index'),
-
-
+    path('orders-list/', views.OrdersListView.as_view(), name='orders_list'),
+    path('create/', views.CreateOrdersView.as_view(), name='create_order'),
+    path('<int:pk>/update/', views.UpdateOrdersView.as_view(), name='modify'),
+    path('<int:pk>/deactivate/', views.deactivate_orders, name='deactivate'),
+    path('<int:pk>/activate/', views.activate_orders, name='activate'),
+    path('<int:pk>/delete/', views.DeleteOrdersView.as_view(), name='delete'),
 ]
