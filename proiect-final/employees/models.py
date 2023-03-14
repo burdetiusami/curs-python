5fef57e91b09298e5ccefeb26d153bd4dfeb88bf
burdetiusami <120407258+burdetiusami@users.noreply.github.com>
2023-03-14 18:58:36 +0100

    Delete proiect-final directory

--- proiect-final/employees/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class Employees(models.Model):
-    first_name = models.CharField(max_length=80)
-    last_name = models.CharField(max_length=80)
-    date_of_birth = models.DateField()
-    email_adress = models.EmailField(max_length=50)
--- proiect-final/orders/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class Orders(models.Model):
-    company = models.CharField(max_length=80)
-    address = models.CharField(max_length=80)
-    cargo_info = models.CharField(max_length=100)
-    eta = models.DateField()
-    status = models.BooleanField(default=True)
